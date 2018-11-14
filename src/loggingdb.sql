06614f0b02a9c9bb151795a5e2fe747daaaaeed5
陈宏葆 <chenhongbao@outlook.com>
2018-11-14 20:51:54 +0800

    增加数据库表的索引，提高检索速度

--- src/loggingdb.sql
+++ src/loggingdb.sql
@@ -3,11 +3,13 @@ CREATE DATABASE loggingdb
     COLLATE utf8mb4_unicode_ci;
     
 CREATE TABLE log_ws_01 (
-	`TimeStamp` CHAR(255) COMMENT 'Time stamp for the logging',
-	`Level` CHAR(255) COMMENT 'Logging level used by java.util.logging.Logger',
-    `LoggerName` CHAR(255) COMMENT 'Logger name used by java.utl.logging.Logger',
-    `Message` TINYTEXT COMMENT 'Logging message sent by java.util.logging.Logger, utf-8',
-    `Millis` BIGINT COMMENT 'Logging time in milliseconds since 1970',
-    `SourceClassName` CHAR(255) COMMENT 'Class that sent the log',
-    `SourceMethodName` CHAR(255) COMMENT 'Method that sent the log'
+	`TimeStamp` CHAR(255) NOT NULL COMMENT 'Time stamp for the logging',
+	`Level` CHAR(255) NOT NULL COMMENT 'Logging level used by java.util.logging.Logger',
+    `LoggerName` CHAR(255) NOT NULL COMMENT 'Logger name used by java.utl.logging.Logger',
+    `Message` TINYTEXT NOT NULL COMMENT 'Logging message sent by java.util.logging.Logger, utf-8',
+    `Millis` BIGINT NOT NULL COMMENT 'Logging time in milliseconds since 1970',
+    `SourceClassName` CHAR(255) NOT NULL COMMENT 'Class that sent the log',
+    `SourceMethodName` CHAR(255) NOT NULL COMMENT 'Method that sent the log',
+    KEY BTREEHASH(`Millis`),
+    KEY HASH(`SourceClassName`, `SourceMethodName`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
